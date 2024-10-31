7c0539f75c35f21bfc8995d112cf3a30b163357c
bokyum <95bogyeom@gmail.com>
2024-10-31 22:56:21 +0900

    navbar scroll threashold 추가

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,4 +1,3 @@
-"use client";
 import TopBar from "../components/TopBar";
 import React from "react";
 
@@ -7,30 +6,9 @@ export default function Layout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [showNav, setShowNav] = React.useState(true);
-  const [lastScrollY, setLastScrollY] = React.useState(0);
-
-  React.useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-      if (currentScrollY > lastScrollY) {
-        setShowNav(false); // 스크롤 다운 시 숨김
-      } else {
-        setShowNav(true); // 스크롤 업 시 표시
-      }
-      setLastScrollY(currentScrollY);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [lastScrollY]);
-
   return (
     <div>
-      <TopBar showNav={showNav} />
+      <TopBar />
 
       {children}
     </div>
--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -1,4 +1,31 @@
-export default function TopBar({ showNav }: { showNav: boolean }) {
+"use client";
+import React from "react";
+
+export default function TopBar() {
+  const [showNav, setShowNav] = React.useState(true);
+  const [lastScrollY, setLastScrollY] = React.useState(0);
+
+  React.useEffect(() => {
+    // 스크롤 기준 설정
+    const scrollThreshold = 100;
+
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > lastScrollY && currentScrollY > scrollThreshold) {
+        setShowNav(false); // 스크롤 다운 시 숨김
+      } else {
+        setShowNav(true); // 스크롤 업 시 표시
+      }
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
     <div
       className={`fixed z-10 top-0 p-4 w-full bg-slate-950 text-white transition-transform duration-300 ${
